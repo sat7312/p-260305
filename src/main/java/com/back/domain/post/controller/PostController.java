79b0b11c41fd73cd1ba254084298bbaf0298f467
sat7312 <sat7312@gmail.com>
2026-03-06 11:29:59 +0900

    17 타임리프 도입, write.html을 이용해 글 등록 페이지 렌더링

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -23,9 +22,8 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts/write-form")
-    @ResponseBody
     public String writeForm() {
-        return getWriteForm("", "", "");
+        return "write";
     }
 
     @AllArgsConstructor
@@ -41,7 +39,6 @@ public class PostController {
     }
 
     @PostMapping("/posts/write")
-    @ResponseBody
     public String write(@Valid WriteRequestForm form, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
@@ -55,33 +52,12 @@ public class PostController {
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
-            return getWriteForm(errorMessages, form.title, form.content);
+            // 템플릿 응답
+            return "write";
         }
 
         Post post = postService.write(form.title, form.content);
-        return "%d번 글이 작성되었습니다.".formatted(post.getId());
-    }
 
-    private String getWriteForm(String errorMessage, String title, String content) {
-        return """
-                <div style="color:red">%s</div>
-                <form method="post" action="/posts/write">
-                  <input type="text" name="title" value="%s", autoFocus>
-                  <br>
-                  <textarea name="content">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                
-                <script>
-                   const li = document.querySelector("ul li");
-                   const errorFieldName = li.dataset.errorField;
-                
-                   if(errorFieldName.length > 0) {
-                       const form = document.querySelector("form");
-                       form[errorFieldName].focus();
-                   }
-                </script>
-                """.formatted(errorMessage, title, content);
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 }
