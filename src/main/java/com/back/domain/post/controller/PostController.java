343486daef32ffc56159c7ab59e546cf90e8aabd
sat7312 <sat7312@gmail.com>
2026-03-09 10:28:37 +0900

    32 글 목록 json으로 출력

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -11,10 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -51,6 +50,12 @@ public class PostController {
 
         model.addAttribute("id", post.getId());
         return "redirect:/posts/%d".formatted(post.getId()); // GET 요청
+    }
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public List<Post> list() {
+        return postService.findAll();
     }
 
     @GetMapping("/posts/{id}")
