4a4d566b65754b05922b8257f35e3c2c370f4edb
sat7312 <sat7312@gmail.com>
2026-03-09 15:53:04 +0900

    45 댓글 수정 기능 구현

--- src/main/java/com/back/domain/post/comment/controller/CommentController.java
+++ src/main/java/com/back/domain/post/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.comment.controller;
 
+import com.back.domain.post.comment.entity.Comment;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import jakarta.validation.Valid;
@@ -8,8 +9,10 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +47,41 @@ public class CommentController {
     public String delete(@PathVariable int postId, @PathVariable int commentId) {
         Post post = postService.findById(postId).get();
         post.deleteComment(commentId);
+
+        return "redirect:/posts/%d".formatted(post.getId());
+    }
+
+    @GetMapping("/posts/{postId}/comments/{commentId}/modify")
+    public String modify(@PathVariable int postId,
+                         @PathVariable int commentId,
+                         Model model
+    ) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
+        model.addAttribute("comment", comment);
+        model.addAttribute("post", post);
+
+        return "comment_modify";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyRequestForm {
+        @NotBlank(message = "01-content-댓글 내용은 필수입니다.")
+        @Size(min = 2, max = 100, message = "02-content-댓글 내용은 2자 이상 100자 이하로 입력해주세요.")
+        private String content;
+    }
+
+    @PostMapping("/posts/{postId}/comments/{commentId}/modify")
+    @Transactional
+    public String modify(
+            @PathVariable int postId,
+            @PathVariable int commentId,
+            @Valid ModifyRequestForm form
+    ) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
+        comment.update(form.content);
 
         return "redirect:/posts/%d".formatted(post.getId());
     }
--- src/main/java/com/back/domain/post/comment/entity/Comment.java
+++ src/main/java/com/back/domain/post/comment/entity/Comment.java
@@ -17,4 +17,8 @@ public class Comment extends BaseEntity {
 
     @ManyToOne
     private Post post;
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
