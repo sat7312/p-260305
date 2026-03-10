af8933e120ff361756a516de67914002b82d9969
sat7312 <sat7312@gmail.com>
2026-03-10 11:05:10 +0900

    47 hidden method로 post, put, delete 분기

--- src/main/java/com/back/domain/post/comment/controller/CommentController.java
+++ src/main/java/com/back/domain/post/comment/controller/CommentController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -66,7 +67,7 @@ public class CommentController {
         String content
     ) {}
 
-    @PostMapping("/posts/{postId}/comments/{commentId}/modify")
+    @PutMapping("/posts/{postId}/comments/{commentId}/modify")
     @Transactional
     public String modify(
             @PathVariable int postId,
